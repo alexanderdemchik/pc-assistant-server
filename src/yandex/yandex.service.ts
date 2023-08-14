41ef922d538e18f79671bfd38c11545f46b96828
Aliaksandr Dzemchyk <aliaksandr.dzemchyk@inspirebrands.com>
2023-08-14 20:15:47 +0300

    update docker node version, swith from got to axios

--- src/yandex/yandex.service.ts
+++ src/yandex/yandex.service.ts
@@ -1,19 +1,26 @@
-import got from 'got';
+import axios from 'axios';
 import { IAliceRequest, IYandexUserInfo } from './yandex.types';
 import { getConnectedUserDevicesIds, sendCommandToDevice } from '../ws';
 import { buildAliceResponse } from './yandex.helper';
 import { getUserById } from '../user/user.service';
 import logger from '../logger';
 
-const YANDEX_USER_INFO_URL = 'https://login.yandex.ru/info?&format=json';
+const YANDEX_USER_INFO_URL = 'https://login.yandex.ru/info';
 
-export function getUserInfo(accessToken: string): Promise<IYandexUserInfo> {
-    logger.info(accessToken);
-    return got
-        .get(YANDEX_USER_INFO_URL, {
-            headers: { Authorization: `OAuth ${accessToken}` },
-        })
-        .json<IYandexUserInfo>();
+export async function getUserInfo(accessToken: string): Promise<IYandexUserInfo> {
+    const res = await axios.post(
+        `${YANDEX_USER_INFO_URL}`,
+        {},
+        {
+            headers: {
+                Authorization: 'OAuth ' + accessToken,
+            },
+        }
+    );
+
+    logger.debug('%o', res.data);
+
+    return res.data;
 }
 
 export async function handleAliceRequest(body: IAliceRequest, userId: string): Promise<any> {
